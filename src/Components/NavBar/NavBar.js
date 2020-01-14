f442a060bb23c2b3d4009b9a0a86d8b774866bb4
Alexandre Gerez <nextriesh@gmail.com>
2020-01-14 20:15:29 +0100

    Full Navbar responsive

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,34 +1,56 @@
 import React from 'react';
 import "./NavBar.css";
+import "./NavBar_Responsive.css";
 
 //Components
 import Button from "../Button/Button.js";
 
-export default function NavBar()
+//Images
+import MenuIcon from "../../assets/icons/menu.png";
+
+export default class NavBar extends React.Component
 {
-    return (
-        <div className="navBarContainer">
-            <div className="navBarContent">
-                <div className="logo">
-                    <h2 className="regularText italic">
-                        Alexandre_<span className="coloredText normal">Gerez</span>
-                    </h2>
-                </div>
-                <div className="navLinks regularText normal">
-                    <a href="#" className="navLink regularText">
-                        {"</Home>"}
+    displayNavBar(responsiveMenu)
+    {
+        if (!responsiveMenu)
+            return ;
+        var isMenuVisible = window.getComputedStyle(responsiveMenu).right === "0px";
+
+        if (!isMenuVisible)
+            responsiveMenu.style.right = "0px";
+        else
+            responsiveMenu.style.right = "-400px";
+    }
+
+    render()
+    {
+        return (
+            <div className="navBarContainer">
+                <div className="navBarContent">
+                    <div className="logo">
+                        <h2 className="regularText italic">
+                            Alexandre_<span className="coloredText normal">Gerez</span>
+                        </h2>
+                    </div>
+                    <a className="navResponsiveButton" onClick={() => {this.displayNavBar(this.refs.navMenu)}}>
+                        <img src={MenuIcon} />
                     </a>
-                    <a href="#" className="navLink regularText">
-                        {"</About_Me>"}
-                    </a>
-                    <a href="#" className="navLink regularText">
-                        {"</Projects>"}
-                    </a>
-                    <a className="navLink">
-                        <Button title="</Contact Me>" link="mailto:pro@alexandregerez.com"/>
-                    </a>
+                    <div ref="navMenu" className="navLinks regularText normal">
+                        <a href="#" className="navLink regularText">
+                            {"</Home>"}
+                        </a>
+                        <a href="#" className="navLink regularText">
+                            {"</About_Me>"}
+                        </a>
+                        <a href="#" className="navLink regularText">
+                            {"</Projects>"}
+                        </a>
+                        <a className="navLink">
+                            <Button title="</Contact Me>" link="mailto:pro@alexandregerez.com"/>
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        ); 
+    }
 }
