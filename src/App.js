91df3498ffa62e40e0a09107cb9615612ffa4720
Alexandre Gerez <nextriesh@gmail.com>
2020-01-16 09:45:12 +0100

    Fixing about height for responsive

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ export default class App extends React.Component {
           <div className="homeContainer">
             <Header />
             <About />
+            <Header />
           </div>
       </div>
     );
