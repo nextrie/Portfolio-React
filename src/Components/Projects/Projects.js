e0b40f27c9907f4a3d54d4e34db80f4a655317b7
Alexandre Gerez <nextriesh@gmail.com>
2020-01-18 12:59:58 +0100

    adding project

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -9,6 +9,7 @@ import Project from "./Project/Project.js";
 
 //Images
 import Kick from "../../assets/images/kick.png";
+import DiscordInvites from "../../assets/images/discordinvitesv2.png";
 
 export default function Projects()
 {
@@ -18,7 +19,7 @@ export default function Projects()
             <div className="project">
                 <Project 
                     title="KickApp"
-                    description="Kick is a phone app based on the ionic framework and angular. My role in this project was to create from scratch the app website using React."
+                    description="Kick is a phone app based on the Ionic framework and Angular. My role in this project was to create from scratch the app website using React."
                     tags={["React.JS,", "HTML,", "CSS"]}
                     img={Kick}
                     github={"https://github.com/nextrie/KickApp-Web"}
@@ -27,12 +28,12 @@ export default function Projects()
             </div>
             <div className="project">
                 <Project 
-                    title="KickApp"
-                    description="Kick is a phone app based on the ionic framework and angular. My role in this project was to create from scratch the app website using React."
-                    tags={["React.JS,", "HTML,", "CSS"]}
-                    img={Kick}
-                    github={"https://github.com/nextrie/KickApp-Web"}
-                    website={"http://kickapp.fr"}
+                    title="DiscordInvites V2"
+                    description="DiscordInvites is a discord server advertising website. I was hired to rewrite from scratch the entire website front end using jQuery and native html, css. I do not have the permissions to make the source code public."
+                    tags={["jQuery,", "HTML,", "CSS"]}
+                    img={DiscordInvites}
+                    github={null}
+                    website={"https://discordinvites.net"}
                 />
             </div>
             <div className="project">
