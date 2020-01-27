5217c33f0042bb15894e938261957e62dd168b78
Alexandre Gerez <nextriesh@gmail.com>
2020-01-27 21:24:29 +0100

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,6 +2,9 @@ import React from 'react';
 import "./Footer.css";
 import "./Footer_Responsive.css";
 
+//Components
+import Button from "../Button/Button.js";
+
 export default function Footer()
 {
     return (
@@ -12,13 +15,13 @@ export default function Footer()
                         <a className="regularText nodecoration" href="#headerContainer">Alexandre Gerez</a>
                     </h3>
                     <h4 className="regularText">
-                        <a className="regularText nodecoration" target="_blank" href="mailto:pro@alexandregerez.com">Contact Me</a>
-                    </h4>
-                    <h4 className="regularText">
                         <a className="regularText nodecoration" target="_blank" href="https://github.com/nextrie">Github</a>
                     </h4>
                     <h4 className="regularText">
                         <a className="regularText nodecoration">+33 7 82 68 72 94</a>
+                    </h4>
+                    <h4>
+                        <Button title="Contact Me" link={"mailto:pro@alexandregerez.com"} />
                     </h4>
                 </div>
             </div>
